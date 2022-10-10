f3785d723a091a70e6c70971a44e8bf2ec7728a9
AndreuVM <andreuvaleria25@gmail.com>
2022-10-10 10:48:35 +0200

    Update agent_profunditat.py

--- quiques/agent_profunditat.py
+++ quiques/agent_profunditat.py
@@ -18,4 +18,29 @@ class BarcaProfunditat(Barca):
     def actua(
             self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
+        estat_inicial = Estat(percep.to_dict())
+
         pass
+
+
+    def cerca(self, estat_inicial: Estat):
+        self.__oberts = [estat_inicial]
+        self.__tancats = []
+        while self.__oberts:
+            estat_actual = self.__oberts.pop()
+            if estat_actual.es_meta():
+                break
+            else:
+                list = estat_actual.genera_fill()
+                self.__tancats.append(estat_actual)
+                for i in list:
+                    self.__oberts.append(i)
+        if estat_inicial.es_meta(): #devuelve el padre y la accion del hijo
+            estat_pare = Estat(estat_inicial.pare.to_dict())
+            while estat_pare is not None:
+                self.__accions.append(estat_pare.acc_poss)
+                estat_pare = Estat(estat_inicial.pare.to_dict())
+            self.__accions.reverse()    
+        pass
+                
+
